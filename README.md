17ebd5db05eda3c1b8b5c7e5b4a459679d0a46b1
ItsMCB <33915290+ItsMCB@users.noreply.github.com>
2021-08-12 18:26:36 -0500

    Test commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruxlit Java Library
+A basic yet useful library that gives access to simple utilities for Java projects.
+
---
+++ src/main/java/me/itsmcb/RuxlitLib.java
@@ -0,0 +1,7 @@
+package me.itsmcb;
+
+public class RuxlitLib {
+    public static String getVersion() {
+        return "@version@";
+    }
+}
